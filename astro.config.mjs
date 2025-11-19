914f111b78552a50527bb4556f043cda33d6b018
Halim Yalçınkaya <abh.yalcinkaya@gmail.com>
2025-11-19 11:13:53 +0100

    updated site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-github-username.github.io',
+  site: 'https://abhyka.github.io',
   base: '/02-astro-github-pages/',
 });
